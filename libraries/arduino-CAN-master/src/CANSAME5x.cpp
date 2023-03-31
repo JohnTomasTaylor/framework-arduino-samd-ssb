dfcd71034c97c0b71917e9e13c76d7893a0d28d9
John Taylor <john.taylor@epfl.ch>
2023-03-31 16:47:05 +0200

    fix for can0, version updated to 1.7.11

--- libraries/arduino-CAN-master/src/CANSAME5x.cpp
+++ libraries/arduino-CAN-master/src/CANSAME5x.cpp
@@ -600,5 +600,5 @@ extern "C" __attribute__((externally_visible)) void CAN1_Handler() {
 
 CANSAME5x *CANSAME5x::instances[2];
 
-CANSAME5x can0;
+CANSAME5x can0(PIN_CAN0_TX, PIN_CAN0_RX);
 CANSAME5x can1(PIN_CAN1_TX, PIN_CAN1_RX);
